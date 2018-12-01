40bee7809f710f08896d1110ee877448692a96ce
dio-ubuntu <hcgcarry@gmail.com>
2018-12-01 22:16:33 +0800

    ha

---
+++ readFileYoutubeWin.py
@@ -0,0 +1,11 @@
+from pytube import YouTube
+filename="musicList.txt"
+file=open(filename,"r")
+
+for videoURL in file:
+    print(videoURL)
+    yt = YouTube(videoURL)
+    stream = yt.streams.first()
+    stream.download("./")
+    print('下載完成')
+    #print("輸入網址錯誤")
---
+++ readme.txt
@@ -0,0 +1,2 @@
+將要下載的歌的網址一行一行的貼到musicList.txt裏面
+執行python readFileYoutubeWin.py
